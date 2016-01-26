67a48887c26221c38248a4ab2f4188e9af6ee822
BPLeon123 <benleon@msn.com>
2016-01-26 08:58:52 -0800

    added "sign up for newsletter section"

--- src/final/home.php
+++ src/final/home.php
@@ -13,7 +13,9 @@ require("header.php");
     <body>
         <h1>Home</h1>
         <p>Welcome to DorothyBiagioni.com, home of dorothy///////////////</p>
-		<h3>Current Freebie:</h3>
+		<h2>Current Freebie:</h2>
+    </body>
+</html>
 <?php
 $prodcatsql = "SELECT * FROM products WHERE freebie = 1";
 $prodcatres = mysqli_query($mysqli,$prodcatsql) or die(mysqli_error($mysqli));
@@ -26,6 +28,7 @@ if($numrows == 0)
 }
 
 else
+	
 {
     echo "<table cellpadding='10'>";
     while($prodrow = mysqli_fetch_assoc($prodcatres))
@@ -34,8 +37,38 @@ else
         if(!empty($prodrow['image'])) 
         {
             echo "<td><img src='./images/" . $prodrow['image']. "' alt='". $prodrow['name'] . "'></td>";
+		}
+		echo "<td>";
+    echo "<form action='addtobasket.php?id=". $prodrow['id'] . "' method='POST'>";
+    echo "</form>";
+    echo "<h2>" . $prodrow['name'] . "</h2>";
+    echo "<p>" . $prodrow['description']. "</p>";
+	echo "</td>";
+	echo "</tr>";
+	}
+	echo "</table>";
+}
+?>
 
-			require("footer.php");
-?>		
+<html>
+    <body>
+        <h2>Sign up for our newsletter</h2>
+        <p>receive weekly E-mails from Dorothy about the latest news and offers!</p>
+        <form action="login.php" method="POST">
+            <table>
+                <tr>
+                    <td>Email</td>
+                    <td><input type="textbox" name="email">
+                </tr>
+                <tr>
+                    <td></td>
+                    <td><input type="submit" name="submit" value="Sign Up">
+                </tr>
+            </table>
+        </form>
     </body>
 </html>
+
+<?php
+require("footer.php");
+?>
