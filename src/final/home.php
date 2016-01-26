d24b717f7bbd6b5799ad104a1a0a168bbe64a006
BPLeon123 <benleon@msn.com>
2016-01-26 10:08:27 -0800

    added social media buttons

--- src/final/home.php
+++ src/final/home.php
@@ -11,9 +11,22 @@ require("header.php");
     </head>
     
     <body>
+        
+        <table cellpadding="1">
+            <tr>
+                <td style="padding-left:20px;padding-right:20px;"><a href="https://www.facebook.com/EFTDorothy?ref=br_rs">
+                        <img src="./images/facebook.jpg" alt="Link to Dorothy's facebook page" width="100" height="50"</a></td>
+                <td style="padding-left:20px;padding-right:20px;"><a href="https://www.youtube.com/channel/UCT2BE3NbSFZPZpYKja4EM5g">
+                        <img src="./images/youtube.jpg" alt="Link to Dorothy's youtube page" width="100" height="50"</a></td>
+                <td style="padding-left:20px;padding-right:20px;"><a href="https://twitter.com/eftdorothy">
+                        <img src="./images/twitter.png" alt="Link to Dorothy's Twitter page" width="100" height="50"</a></td>         
+                <td style="padding-left:20px;padding-right:20px;"><a href="https://www.linkedin.com/in/dorothybiagioni">
+                        <img src="./images/LI.png" alt="Link to Dorothy's LinkedIn page" width="100" height="50"</a></td>
+            </tr>
+        </table>
         <h1>Home</h1>
-        <p>Welcome to DorothyBiagioni.com, home of dorothy///////////////</p>
-		<h2>Current Freebie:</h2>
+        <p>Welcome to DorothyBiagioni.com, home of Dorothy///////////////</p><br/>
+	<h2>Current Freebie:</h2>
     </body>
 </html>
 <?php
@@ -48,27 +61,29 @@ else
 	}
 	echo "</table>";
 }
-?>
 
-<html>
-    <body>
-        <h2>Sign up for our newsletter</h2>
-        <p>receive weekly E-mails from Dorothy about the latest news and offers!</p>
-        <form action="login.php" method="POST">
-            <table>
-                <tr>
-                    <td>Email</td>
-                    <td><input type="textbox" name="email">
-                </tr>
-                <tr>
-                    <td></td>
-                    <td><input type="submit" name="submit" value="Sign Up">
-                </tr>
-            </table>
-        </form>
-    </body>
-</html>
-
-<?php
+if(!isset($_SESSION['SESS_LOGGEDIN'])){
+//echo "<html>";
+//echo "<body>";
+echo "<br/>";
+echo "<h2>Sign up for our newsletter</h2>";
+echo "<p>receive weekly E-mails from Dorothy about the latest news and offers!</p>";
+echo "<form action='login.php' method='POST'>";
+echo "<table>";
+echo "<tr>";
+echo "<td>Email</td>";
+echo "<td><input type='textbox' name='email'>";
+echo "</tr>";
+echo "<tr>";
+echo "<td></td>";
+echo "<td><input type='submit' name='submit' value='Sign Up'>";
+echo "</tr>";
+echo "</table>";
+echo "</form>";
+//echo "</body>";
+//echo "</html>";
+}
+else{
+}
 require("footer.php");
 ?>
